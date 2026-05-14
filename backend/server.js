d3d8ce64302cbdee8e7d1c17392ca88316e7ca5f
ziaddiaa01 <98720925+ziaddiaa01@users.noreply.github.com>
2026-05-14 17:17:04 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -62,5 +62,7 @@ mongoose.connect(process.env.MONGO_URI)
     process.exit(1); // Exit if database connection fails
   });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+}
