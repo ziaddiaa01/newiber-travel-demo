8e7d59752f42928c899e9885572d66472b80bc1d
ziaddiaa01 <98720925+ziaddiaa01@users.noreply.github.com>
2026-05-21 21:40:52 +0300

    Update SocialIcons.jsx

--- frontend/src/components/SocialIcons.jsx
+++ frontend/src/components/SocialIcons.jsx
@@ -1,4 +1,4 @@
-import { FaWhatsapp, FaFacebookF, FaInstagram, FaTiktok } from 'react-icons/fa';
+import { FaWhatsapp, FaFacebookF, FaInstagram, FaTiktok } from 'react-icons/fa6';
 
 
 const SocialIcons = ({ variant }) => {
