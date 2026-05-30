d98e8cf13087d5cff0cafc037b76d9eb0a059538
ziaddiaa01 <98720925+ziaddiaa01@users.noreply.github.com>
2026-05-30 04:17:40 +0300

    Update publicRoutes.js

--- backend/routes/publicRoutes.js
+++ backend/routes/publicRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { getServices, getServiceById, getTestimonials, getFAQs } = require('../controllers/publicController');
+const { getServices, getServiceById, getTestimonials, getFAQs , getDestinations} = require('../controllers/publicController');
 const router = express.Router();
 
 router.get('/services', getServices);
 router.get('/services/:id', getServiceById);
 router.get('/testimonials', getTestimonials);
 router.get('/faqs', getFAQs);
+router.get('/destinations', getDestinations);
+
+
 
 module.exports = router;
