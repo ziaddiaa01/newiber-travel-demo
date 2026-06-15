1c0d4fc39986c9b29495594f347759a4e619ca6f
ziaddiaa01 <98720925+ziaddiaa01@users.noreply.github.com>
2026-06-15 15:14:53 +0300

    Update imageRegistry.js

--- frontend/src/utils/imageRegistry.js
+++ frontend/src/utils/imageRegistry.js
@@ -2,15 +2,15 @@
 import hotel from '../assets/hotel.png';
 import managment from '../assets/managment.png';
 import limousine from '../assets/limousine.png';
-import ship from '../assets/ship.png';
+import ship from 'https://ymvejvzrljpnnwfautji.supabase.co/storage/v1/object/public/NEWIBER/ship.png';
 import faq1 from 'https://ymvejvzrljpnnwfautji.supabase.co/storage/v1/object/public/NEWIBER/faq1.jpg';
 import faq2 from 'https://ymvejvzrljpnnwfautji.supabase.co/storage/v1/object/public/NEWIBER/faq2.jpg';
 import faq3 from 'https://ymvejvzrljpnnwfautji.supabase.co/storage/v1/object/public/NEWIBER/faq3.jpg';
 import faq4 from 'https://ymvejvzrljpnnwfautji.supabase.co/storage/v1/object/public/NEWIBER/faq4.jpg';
 
-import Aviation from '../assets/vip1.jpg';
-import Maritime from '../assets/vip2.jpg';
-import Private from '../assets/vip3.jpg';
+import Aviation from 'https://ymvejvzrljpnnwfautji.supabase.co/storage/v1/object/public/NEWIBER/vip1.jpg';
+import Maritime from 'https://ymvejvzrljpnnwfautji.supabase.co/storage/v1/object/public/NEWIBER/vip2.jpg';
+import Private from 'https://ymvejvzrljpnnwfautji.supabase.co/storage/v1/object/public/NEWIBER/vip3.jpg';
 
 // Map the DB string to the imported variable
 export const masterImageMap = {
